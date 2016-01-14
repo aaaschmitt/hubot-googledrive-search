4bbeaf75192e1221ee95ad9a145fa6066bbe8bbd
Travis CI <aschmitt@berkeley.edu>
2016-01-14 03:13:01 -0500

    switched over to using package hubot-google-auth

--- src/hubot-googledrive-search.js
+++ src/hubot-googledrive-search.js
@@ -6,7 +6,7 @@
 //   requires a hubot brain
 //
 // Commands:
-//   hubot drive code <code>  
+//   hubot drive set code <code>  
 //          -  used to authenticate the bot initially (see setCode and generateAuthUrl)
 //
 //   hubot drive tokens  
@@ -24,28 +24,19 @@
 // Author:
 //  Andrew Schmitt
 
-var google = require('googleapis');
-var drive = google.drive('v2');
+var CLIENT_ID = process.env.HUBOT_DRIVE_CLIENT_ID,
+    CLIENT_SECRET = process.env.HUBOT_DRIVE_CLIENT_SECRET,
+    REDIRECT_URL = process.env.HUBOT_DRIVE_REDIRECT_URL,
+    SCOPES = 'https://www.googleapis.com/auth/drive';
 
-// Make sure that these keys do not conflict with things that are already in your hubot's brain
-var TOKEN_KEY = 'HUBOT_DRIVE_AUTH_TOKEN',
-    REFRESH_KEY = 'HUBOT_DRIVE_REFRESH_TOKEN',
-    EXPIRY_KEY = 'HUBOT_DRIVE_EXPIRE_TIME';
-
-var CLIENT_ID = process.env.DRIVE_CLIENT_ID,
-    CLIENT_SECRET = process.env.DRIVE_CLIENT_SECRET,
-    REDIRECT_URL = process.env.DRIVE_REDIRECT_URL;
-
-// Initialize the oauthClient
-var OAuth2 = google.auth.OAuth2;
-var oauthClient = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
-google.options({
-    auth: oauthClient
-});
+// We'll initialize the google auth in the module.exports functions
+// since this is when we have access to the brain
+var HubotGoogleAuth = require('hubot-google-auth');
+var auth;
 
 /**
  * Lists files and links for the given queryString
- * For more info on constructing query string see: https://developers.google.com/drive/v2/web/search-parameters
+ * For more info on constructing query strings see: https://developers.google.com/drive/v2/web/search-parameters
  * This will not handle any paging and will only return the top search results given by the drive api
  *
  * @param  query     a query string
@@ -53,16 +44,31 @@ google.options({
  */
 var search = function(queryString, cb) {
 
-    validateToken(function(err, resp) {
+    var tokens = auth.getTokens();
+
+    // If there are no existing refresh tokens then the user will need to manually authenticate
+    if (!tokens.refresh_token) {
+        var authUrl = auth.generateAuthUrl(),
+            authMsg = `Authorize this app by visiting this url :\n ${authUrl}` +
+                    '\nThen use @hubot drive set code <code>';
+
+        cb({
+            err: null,
+            msg: authMsg
+        });
+        return;
+    }
+
+    auth.validateToken(robot.brain, function(err, resp) {
         if (err) {
             cb({
                 err: err,
-                msg: err.msg
+                msg: authMsg
             });
             return;
         }
 
-        drive.files.list({
+        auth.google.drive('v2').files.list({
             q: queryString
         }, function(err, resp) {
             if (err) {
@@ -106,125 +112,24 @@ var formatSearchRes = function(results) {
     var res = '';
     // limit to 5 results
     for (var i = 0; i < 5 && i < results.length; i++) {
-        res += `Name: ${results[i].title} Link: ${results[i].alternateLink}\n`;
+        res += `#{i+1}.) ${results[i].title} \n${results[i].alternateLink}\n\n`;
     }
 
     return res;
-}
-
-/**
- * Stores the token and expire time into the robot brain and
- * Sets it in the oauthClient
- *
- * @param  token  the token object returned from google oauth2
- */
-var storeToken = function(token) {
-    oauthClient.setCredentials(token);
-    robot.brain.set(TOKEN_KEY, token.access_token);
-    if (token.refresh_token) {
-        robot.brain.set(REFRESH_KEY, token.refresh_token);
-    }
-    robot.brain.set(EXPIRY_KEY, +token.expiry_date);
-    robot.brain.save();
-    robot.brain.resetSaveInterval(60);
-}
-
-/**
- * Initially tokens must be created from the command line.
- * This requires a user manually inputting a code so it cannot be done by the bot alone.
- * This generates the url where the code can be obtained
- */
-var generateAuthUrl = function() {
-    var scopes = [
-        'https://www.googleapis.com/auth/drive'
-    ];
-    var authUrl = oauthClient.generateAuthUrl({
-        access_type: 'offline', //offline means that we get a refresh token
-        scope: scopes
-    });
-
-    return authUrl;
-}
-
-/**
- * Used to set the code provided by the generated auth url. 
- * This code is generated for a user and is needed to initiate the oauth2 handshake.
- *
- * @param  code  the code obtained by a user from the auth url
- */
-var setCode = function(code, cb) {
-    oauthClient.getToken(code, function(err, token) {
-        if (err) {
-            console.log(err);
-            cb({
-                err: err,
-                msg: 'Error while trying to retrieve access token'
-            });
-            return;
-        }
-        storeToken(token);
-        cb(null, {
-            resp: token,
-            msg: "Drive code successfully set"
-        });
-    });
-}
-
-/**
- * Checks the current expire time and determines if the token is valid.
- * Refreshes the token if it is not valid.
- *
- * @param  cb  the callback function (err, resp), use this to make api calls
- */
-var validateToken = function(cb) {
-    var at = robot.brain.get(TOKEN_KEY),
-        rt = robot.brain.get(REFRESH_KEY);
-
-    if (at == null || rt == null) {
-        var authMsg = `Authorize this app by visiting this url :\n ${generateAuthUrl()}` +
-            '\nThen use @hubot drive set code <code>';
-
-        cb({
-            err: null,
-            msg: authMsg
-        });
-        return;
-    }
-
-    var expirTime = robot.brain.get(EXPIRY_KEY),
-        curTime = (new Date()) / 1;
-
-    if (expirTime < curTime) {
-        oauthClient.refreshAccessToken(function(err, token) {
-            if (err != null) {
-                cb({
-                    err: err,
-                    msg: 'Drive Authentication Error: error refreshing token'
-                }, null);
-                return;
-            }
-
-            storeToken(token);
-            cb(null, {
-                resp: token,
-                msg: 'Token refreshed'
-            });
-        });
-    } else {
-        cb(null);
-    }
 }
 
 // Export robot functions
 var initialBrainLoad = true;
 module.exports = function(robot) {
 
+    auth = new HubotGoogleAuth(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL, SCOPES, robot.brain);
+
     robot.respond(/drive(\s+set)?\s+code\s+([^\s]+)/i, {
         id: 'drive.set-code'
     }, function(msg) {
         var code = msg.match[2];
         msg.send('Attempting to set code...')
-        setCode(code, function(err, resp) {
+        auth.setCode(code, function(err, resp) {
             if (err) {
                 msg.send(err.msg);
                 return;
@@ -237,13 +142,10 @@ module.exports = function(robot) {
     robot.respond(/drive tokens/i, {
         id: 'drive.show-tokens'
     }, function(msg) {
-        var tok = robot.brain.get(TOKEN_KEY),
-            ref_tok = robot.brain.get(REFRESH_KEY),
-            expire = robot.brain.get(EXPIRY_KEY);
-
-        msg.send('token: ' + tok);
-        msg.send('refresh token: ' + ref_tok);
-        msg.send('expire date: ' + expire);
+        var tokens = auth.getTokens();
+        for (var name in tokens) {
+            msg.send(`#{name}: ${tokens[name]}`);
+        }
     });
 
     robot.respond(/drive search query=(.)+/i, {
@@ -287,22 +189,6 @@ module.exports = function(robot) {
             }
 
             msg.send(formatSearchRes(resp));
-        });
-    });
-
-    // Set credentials on load. Does not validate/refresh tokens
-    robot.brain.on('loaded', function() {
-        if (!initialBrainLoad) {
-            return;
-        }
-
-        initialBrainLoad = false;
-        var at = robot.brain.get(TOKEN_KEY),
-            rt = robot.brain.get(REFRESH_KEY);
-
-        oauthClient.setCredentials({
-            access_token: at,
-            refresh_token: rt
         });
     });
 }
